4d5d2039794f70683e210c9d0008bc7006756d93
Bonotto Riccardo <rbonotto@chilesotti.it>
2025-11-24 15:35:11 +0100

    maitake maitake guru guru

---
+++ Progetto/script.js
@@ -0,0 +1,21 @@
+function ColonnaPiu() {
+    //variabile temporanea per conteggio giri, in futuro implementata dentro json classe
+    let Guru=2;//maitake maitake guruguruguru
+    
+    let Tabella = document.getElementById("griglia");
+    for (let i = 0; i < Tabella.rows.length; i++) {
+        let cell;
+
+        // If first row → create a header cell
+        if (i === 0) {
+            cell = document.createElement("th");
+            cell.textContent = "giro"+Guru;
+        } else {
+            cell = document.createElement("td");
+            cell.textContent = "Data " + i;
+        }
+
+        Tabella.rows[i].appendChild(cell);
+    }
+    Guru++;
+}
